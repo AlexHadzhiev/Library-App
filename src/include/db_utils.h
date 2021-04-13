36eb9048acbdaf206098080bc2130435687a5c32
AlexHadzhiev <alexhadzhiev2@gmail.com>
2021-04-13 11:38:10 +0300

    Added include guards on both header files

--- src/include/db_utils.h
+++ src/include/db_utils.h
@@ -1,3 +1,6 @@
+#ifndef DB_UTILS_H
+#define DB_UTILS_H
+
 #include <mysql.h>
 
 static int default_collection_set = 0;
@@ -22,3 +25,5 @@ delete_collection ( MYSQL * connection , char ** values );
 
 int
 delete_book_from_collection ( MYSQL * connection , char ** values );
+
+#endif
